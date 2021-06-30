a21798987bf48981a046b8d2e7d708e70d929765
Amjad Oudeh <amjadoudeh@Amjads-M1-MacBook-Air.local>
2021-06-30 09:35:01 +0200

    the data is da :)

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import Row from './Row';
 import requests from './requests';
 
-
-
 function App() {
   return (
-    <div className="App">
-      <Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
-			<Row title="Trending" fetchUrl={requests.fetchTrending} />
-			<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+    <div className='App'>
+      <Row
+        title='NETFLIX ORIGINALS'
+        isLargeRow
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title='Trending' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
     </div>
   );
 }
