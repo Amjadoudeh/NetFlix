07ef6c0fab4e46c5f67e80951487ed6183084aee
Amjad Oudeh <amjadoudeh@Amjads-M1-MacBook-Air.local>
2021-07-11 19:45:46 +0200

    adding more movies and the banner

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import Banner from './Banner';
 import requests from './requests';
 
 function App() {
   return (
     <div className='App'>
+      <Banner />
       <Row
         title='NETFLIX ORIGINALS'
         isLargeRow
         fetchUrl={requests.fetchNetflixOriginals}
       />
       <Row title='Trending' fetchUrl={requests.fetchTrending} />
-      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
